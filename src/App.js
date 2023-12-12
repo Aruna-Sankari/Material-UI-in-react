3e38c5a7807c699e386f4f1dcb47782dc7a2f7cf
Aruna-Sankari <arunaponrajkdm@gmail.com>
2023-12-12 19:44:20 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/// /import logo from './logo.svg';
+import './App.css';
+import MediaControlCard from './Component/Mui.js'
+function App() {
+  return (
+    <div>
+         <MediaControlCard />
+    </div>
+  );
+}
+export default App;
